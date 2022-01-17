1879d204c9b03cbf6ca39060458243ec7af42596
matthewslape <75864370+matthewslape@users.noreply.github.com>
2022-01-16 20:04:40 -0800

    added animation on click

--- script.js
+++ script.js
@@ -8,23 +8,56 @@ const playSound = e => {
     const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
     const key = document.querySelector(`div[data-key="${keyCode}"]`);
 
-    if (!audio) return;
+    if (!audio){
+      key.classList.add('pressed');
+      setTimeout(function() {
+        key.classList.remove('pressed');
+      }, 5000);
+      return;
+    } else {
+      key.classList.add('pressed');
+      audio.currentTime = 0;
+      audio.play();
 
-    key.classList.add('playing');
-    audio.currentTime = 0;
-    audio.play();
-  }
+      //audio.addEventListener('ended', removeTransition(key));
+      //audio.onended = removeTransition(key);
+    }
+}
 
-  const removeTransition = e => {
-    if (e.propertyName !== 'transform') return;
-    e.target.classList.remove('playing');
-  }
+/*const pressGrow = n => {
+  let keyCode;
+    if (n.type === 'keydown') {
+      keyCode = n.keyCode;
+    } else {
+      keyCode = n.target.getAttribute('data-key') || n.target.parentNode.getAttribute('data-key');
+    }
+  var button = document.querySelector(`div[data-key="${keyCode}"]`);
+  button.classList.add('pressed');
 
+  setTimeout(function() {
+    button.classList.remove('pressed');
+  }, 5000);
+
+}*/
+
+function removeTransition() {
   const keys = Array.from(document.querySelectorAll('.key'));
   keys.forEach(key => {
-    key.addEventListener('transitionend', removeTransition)
-  });
+    if (key.classList.contains('pressed')){
+      key.classList.remove('pressed');
+    } else {
+      return;
+    }
+  })
+}
 
-  window.addEventListener('keydown', playSound);
-  window.addEventListener('touchstart', playSound);
-  window.addEventListener('click', playSound);
+/*const audios = Array.from(document.querySelectorAll('.audio'));
+const keys = Array.from(document.querySelectorAll('.key'));
+
+audios.forEach(audio => {
+  audio.addEventListener('ended', removeTransition(keys))
+});*/
+
+window.addEventListener('keydown', playSound);
+window.addEventListener('touchstart', playSound);
+window.addEventListener('click', playSound);
